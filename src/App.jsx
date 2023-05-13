a6513ca0b34ebb21cc7202ff7bd4260d64c94431
minshantt <min.shantt@gmail.com>
2023-05-13 22:20:38 +0700

    02_addModuleTemplate_Style.js #02_Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <h1>Whatsup</h1>
+    </div>
+  );
 };
 
 export default App;
