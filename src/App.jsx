44b7ab4f4ded4439254711ad440f8181c495feb0
minshantt <min.shantt@gmail.com>
2023-05-14 01:16:01 +0700

    add_indexStyle_gradient_ColorScheme #03_StyleCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from './style.js';
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <h1>Whatsup</h1>
+      <div className={`${styles.paddingX} ${styles.flexCen}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      {/* Main */}
+      <div className={`bg-primary ${styles.flexStrt}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
     </div>
   );
 };
